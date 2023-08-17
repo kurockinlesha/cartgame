0e5627712e713085576fc9fff235d8b2f6662388
Kurochkin Alexey <kurockinaleksej29@gmail.com>
2023-08-17 10:28:04 +0300

    hw.2

---
+++ renderGame.js
@@ -0,0 +1,9 @@
+export default function renderGame() {
+    const app = document.querySelector('#app')
+    const html = `
+        <div>
+            Игровое поле
+        </div>
+    `
+    app.innerHTML = html
+}
